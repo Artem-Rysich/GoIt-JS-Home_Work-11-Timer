2be1aea628d055dee6d3e0cc8d689675d490faab
Artem <56354347+Artem-Rysich@users.noreply.github.com>
2020-05-29 18:59:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 'use strict';
 class CountdownTimer {
-  constructor({ selector, targetDate }) {
-    (this.selector = selector), (this.targetDate = targetDate);
+  constructor({selector, targetDate}) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+
   }
-  startTimer() {
-    setInterval(timer => {
-      const timeToday = new Date();
-      const time = this.targetDate - timeToday;
-      const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(
-        3,
-        '0',
-      );
-      const hours = String(
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      ).padStart(2, '0');
-      const mins = String(
-        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
-      ).padStart(2, '0');
-      const secs = String(Math.floor((time % (1000 * 60)) / 1000)).padStart(
-        2,
-        '0',
-      );
-      this.selector = document.querySelector(
-        '[data-value="days"]',
-      ).textContent = days;
-      this.selector = document.querySelector(
-        '[data-value="hours"]',
-      ).textContent = hours;
-      this.selector = document.querySelector(
-        '[data-value="mins"]',
-      ).textContent = mins;
-      this.selector = document.querySelector(
-        '[data-value="secs"]',
-      ).textContent = secs;
+  startTimer () {
+    const expectedDate = this.targetDate.getTime ();
+    const day = document.querySelector(`${this.selector} [data-value="days"]`);
+    const hour =  document.querySelector(`${this.selector} [data-value="hours"]`);
+    const min = document.querySelector(`${this.selector} [data-value="mins"]`);
+    const sec = document.querySelector(`${this.selector} [data-value="secs"]`);
+    setInterval(() => {
+      const currentDate = new Date ().getTime ();
+      const interval = expectedDate - currentDate;
+      const days = String(Math.floor(interval / (1000 * 60 * 60 * 24))).padStart(3, '0');
+      const hours = String(Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
+      const mins = String(Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+      const secs = String(Math.floor((interval % (1000 * 60)) / 1000)).padStart(2, '0');
+      day.textContent = days;
+      hour.textContent = hours;
+      min.textContent = mins;
+      sec.textContent = secs;
     }, 1000);
   }
 }
@@ -41,3 +30,4 @@ const NewYearTimer = new CountdownTimer({
   targetDate: new Date('December 31, 2020'),
 });
 NewYearTimer.startTimer();
+
